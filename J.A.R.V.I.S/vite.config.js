75727bad6bc4ca596995450d3ae08909e6f7661e
unknown <math2.0@hotmail.com>
2025-11-28 22:17:13 -0300

    shadcn added

--- J.A.R.V.I.S/vite.config.js
+++ J.A.R.V.I.S/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  theme: {
-    extend: {
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  content: [
-    './src/**/*.{html,js,jsx,ts,tsx}',
-    './index.html',
-  ],
 })
