e100bffe89be20acbcb2189843a95a20574ffc3e
Matthew Lacerda <math2.0@hotmail.com>
2025-11-30 10:49:58 -0300

    Debug lines at CanvasDropZone.tsx

--- J.A.R.V.I.S/src/components/canvas/CanvasDropZone.tsx
+++ J.A.R.V.I.S/src/components/canvas/CanvasDropZone.tsx
@@ -18,7 +18,9 @@ export function CanvasDropZone({ onFilesDropped, children }: Readonly<CanvasDrop
       className="relative w-full h-full"
       id="canvas-drop-zone"
     >
+      <b>This shows up on screen</b>
       {children}
+      <b>This does not</b>
       {isDragging && (
         <div className="absolute inset-0 bg-blue-500/20 border-4 border-dashed border-blue-500 flex items-center justify-center z-50">
           <div className="text-purple-600 text-2xl font-semibold">Drop files here</div>
